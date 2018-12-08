ccef2b419503537fde61badef0245f6488688b30
woon9 <woon93@163.com>
2018-12-08 12:44:47 +0800

    emmm01

---
+++ src/main/java/com/union/Controller/Test01.java
@@ -0,0 +1,18 @@
+package com.union.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@org.springframework.stereotype.Controller
+public class Test01 {
+
+    @RequestMapping(value = {"/"})
+    @ResponseBody
+    public String hello(){
+        return "Hello World!";
+    }
+
+    @RequestMapping(value = {"/test01"})
+    public String test01(){
+        return "Test01.html";
+    }
+}
