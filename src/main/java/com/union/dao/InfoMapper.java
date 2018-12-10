c7a6e88e420a232e3c07cc0d53c886082253c92d
RuanJianXinCheng <woon93@163.com>
2018-12-10 21:31:10 +0800

    get Mybatis generator

--- src/main/java/com/union/dao/InfoMapper.java
+++ src/main/java/com/union/dao/InfoMapper.java
@@ -1,9 +1,22 @@
 package com.union.dao;
 
 import com.union.model.Info;
+import com.union.model.InfoExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface InfoMapper {
+    int countByExample(InfoExample example);
+
+    int deleteByExample(InfoExample example);
+
     int insert(Info record);
 
     int insertSelective(Info record);
+
+    List<Info> selectByExample(InfoExample example);
+
+    int updateByExampleSelective(@Param("record") Info record, @Param("example") InfoExample example);
+
+    int updateByExample(@Param("record") Info record, @Param("example") InfoExample example);
 }
