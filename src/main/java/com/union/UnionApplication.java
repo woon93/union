fc689f1bae91fdeb17779f24537a3a1e3c460cd2
RuanJianXinCheng <woon93@163.com>
2018-12-10 20:45:52 +0800

    test02

--- src/main/java/com/union/UnionApplication.java
+++ src/main/java/com/union/UnionApplication.java
@@ -1,9 +1,11 @@
 package com.union;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.union.dao")
 public class UnionApplication {
 
 	public static void main(String[] args) {
