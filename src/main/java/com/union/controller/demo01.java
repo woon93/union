eaccbecc4f2b3740b7b89ec0df99674cf5df25a2
woon9 <woon93@163.com>
2018-12-16 14:47:55 +0800

    test03.5

---
+++ src/main/java/com/union/controller/demo01.java
@@ -0,0 +1,24 @@
+package com.union.controller;
+
+import com.union.service.InfoMstService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+//@RestController
+public class demo01 {
+    @Autowired
+    private InfoMstService infoMstService;
+
+    @GetMapping(value = "/demo01")
+    public String hello(Model model) {
+        String name = "大王";
+        model.addAttribute("name", name);
+        return "Demo01";
+    }
+
+
+
+}
