d0e01ab022adffa618251f12f5aca14b34e969da
RuanJianXinCheng <woon93@163.com>
2019-01-05 17:56:04 +0800

    ueditor例子升级

--- src/main/java/com/union/controller/ForumController.java
+++ src/main/java/com/union/controller/ForumController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -55,6 +56,10 @@ public class ForumController {
         return "Forum";
     }
 
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
 
 
 }
--- src/main/java/com/union/controller/Test/Test01.java
+++ src/main/java/com/union/controller/Test/Test01.java
@@ -8,7 +8,7 @@ public class Test01 {
 
     @RequestMapping(value = {"/"})
     private String showPage(){
-        return "Test01";
+        return "demo/Test01";
     }
 
     @ResponseBody
@@ -18,6 +18,7 @@ public class Test01 {
 
     @RequestMapping(value = {"/test01"})
     public String test01(){
-        return "Test01.html";
+        return "login.html";
     }
+
 }
