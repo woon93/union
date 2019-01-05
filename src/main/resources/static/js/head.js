a9a1d2b65b5eadfd3bfcd7ff45e30b3fbcd931a6
QSX1113 <1070201230@qq.com>
2019-01-05 20:04:36 +0800

    headjsp

--- src/main/resources/static/js/head.js
+++ src/main/resources/static/js/head.js
@@ -5,4 +5,10 @@ $(this).css('color','#0F0')
 $("a").mouseout(function() {
 $(this).css('color','#FFF')
 });
+$("li").mouseover(function() {
+$(this).css('color','#0F0')
+});
+$("li").mouseout(function() {
+$(this).css('color','#FFF')
+});
 });
