01d5cfe1f0e3d5c7213d6f2fba57895a9b3bb9fc
woon9 <woon93@163.com>
2019-02-21 23:44:06 +0800

    picFocus demo

--- src/main/resources/static/js/head.js
+++ src/main/resources/static/js/head.js
@@ -2,13 +2,13 @@ $(function(){
 $("a").mouseover(function() {
 $(this).css('color','#0F0')
 });
-$("a").mouseout(function() {
-$(this).css('color','#FFF')
-});
+//$("a").mouseout(function() {
+//$(this).css('color','#FFF')
+//});
 $("li").mouseover(function() {
 $(this).css('color','#0F0')
 });
-$("li").mouseout(function() {
-$(this).css('color','#FFF')
-});
+//$("li").mouseout(function() {
+//$(this).css('color','#FFF')
+//});
 });
