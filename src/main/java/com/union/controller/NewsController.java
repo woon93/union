b9d932215a034e76557c64a447695a21ba8d7764
RuanJianXinCheng <woon93@163.com>
2019-03-01 21:19:32 +0800

    picFocus test

--- src/main/java/com/union/controller/NewsController.java
+++ src/main/java/com/union/controller/NewsController.java
@@ -4,14 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class TestController {
-    @RequestMapping(value = {"/test"})
+public class NewsController {
+    @RequestMapping(value = {"/news"})
     public String test1(){
         return "news.html";
     }
 
-    @RequestMapping(value = {"/class"})
-    public String test2(){
-        return "class.html";
-    }
 }
