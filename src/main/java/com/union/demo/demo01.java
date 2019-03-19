52bc1a6fb6b017f5f6118e44150343f5c0fe26f7
RuanJianXinCheng <woon93@163.com>
2019-03-19 21:32:25 +0800

    testDemo

---
+++ src/main/java/com/union/demo/demo01.java
@@ -0,0 +1,4 @@
+package com.union.demo;
+
+public class demo01 {
+}
---
+++ src/main/java/demo/demo01.java
@@ -0,0 +1,12 @@
+package demo;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class demo01 {
+     public void main(String[] args){
+//         FileInputStream fileInputStream = new FileInputStream("D:\\testData.txt");
+
+
+     }
+}
